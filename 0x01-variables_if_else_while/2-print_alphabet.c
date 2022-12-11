62397a1e74f557bedeed6b53f0a127b42d12a540
victoria-umoh <victoriasuave07@gmail.com>
2022-12-11 03:58:45 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,16 @@
 /**
  * main - prints the alphabet in lowercase.
  *
- *Return: 0
+ *Return:Always (success)
 */
 
 int main(void)
 {
-	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		char letter;
-	for (letter = 'a'; letter < = 'z'; letter++)
-		putchar(letter);
+		char c
+	for (c = 'a'; c < = 'z'; c++)
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
