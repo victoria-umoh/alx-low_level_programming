16f69425215600e11f250c58cb69bb6dbfa84dfe
victoria-umoh <victoriasuave07@gmail.com>
2022-12-10 22:00:15 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 		char letter;
 	for (letter = 'a'; letter < = 'z'; letter++)
 		putchar(letter);
