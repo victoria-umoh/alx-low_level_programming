1c16221e5ce7364d022d2b53cf71c70ee7956bee
victoria-umoh <victoriasuave07@gmail.com>
2022-12-13 05:28:05 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,8 +2,6 @@
 
 /**
  * times_table - Prints the 9 times table, starting with 0.
- *
- *
  */
 
 void times_table(void)
@@ -12,11 +10,15 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		_putchar(',');
 		_putchar(' ');
+		{
 
 		prod = num * mult;
-		{
+
 		if (prod <= 9)
 			_putchar(' ');
 		else
