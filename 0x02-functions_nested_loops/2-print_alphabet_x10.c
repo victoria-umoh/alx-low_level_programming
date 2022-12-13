0e12b556c7a3b0b02017c4a06e1553464e4bcddb
victoria-umoh <victoriasuave07@gmail.com>
2022-12-13 03:24:09 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 - print 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase.
  *
  * Return: Always 0.
  */
