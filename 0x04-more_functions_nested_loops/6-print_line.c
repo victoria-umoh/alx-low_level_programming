a08f010c94b6c504b06d0f781b23ffe0c1083051
victoria-umoh <victoriasuave07@gmail.com>
2022-12-16 20:36:24 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: the length of the line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
