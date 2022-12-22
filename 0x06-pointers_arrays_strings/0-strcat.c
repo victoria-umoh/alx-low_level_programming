a5b3379e84bd265a84d1d2759fc884bc9663d766
victoria-umoh <victoriasuave07@gmail.com>
2022-12-22 15:18:20 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
  /**
   * *_strcat - concatenation of 2 strings.
@@ -9,16 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int x, y;
+	int  x, y;
 
-	for (x = 0; dest[x] != '\0' x++)
-		;
-	for (y = 0; src[j] != '\0' y++)
+	x = 0;
+	while (dest[x] != '\0')
+		x++;
+	y = 0;
+	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
 		x++;
+		y++;
 	}
-
 	dest[x] = '\0';
 	return (dest);
 }
